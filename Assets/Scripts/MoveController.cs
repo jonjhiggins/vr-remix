f80e146aa2113953122ec9e43991b3eb29c0d7c5
Jon Higgins <jon@jonhiggins.co.uk>
2018-08-28 21:13:13 +1000

    add SteamVR + VRTK

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position -= transform.forward;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position -= transform.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += transform.right;
+        }
+    }
+}
