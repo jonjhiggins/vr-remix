ecd6881b6b15d648454ffb954b34c69855ba93ad
Jon Higgins <jon@jonhiggins.co.uk>
2018-09-01 16:08:13 +1000

    DrawInk - only draw when spacebar held down

---
+++ Assets/Scripts/DrawInk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DrawInk : MonoBehaviour {
+    private TrailRenderer ink;
+
+    void Awake () {
+        // Start with ink disabled
+        ink = gameObject.GetComponent<TrailRenderer>();
+        ink.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        ink.enabled = Input.GetKey(KeyCode.Space);
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            ink.Clear();
+        } 
+    }
+}
