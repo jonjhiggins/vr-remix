d7e34428652521128c572e149528ba402f19e7b7
Jon Higgins <jon@jonhiggins.co.uk>
2018-09-08 11:48:35 +1000

    add debug canvas

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -18,10 +18,12 @@ public class FollowTarget : MonoBehaviour
 
     // Transform controller position 
     private Vector3 GetControllerTransformed(Vector3 targetPosition) {
-        string debugString = targetPosition.x.ToString() + ' ' + targetPosition.y.ToString() + ' ' + targetPosition.z.ToString();
-        Debug.Log(debugString);
+        string debugString = targetPosition.x.ToString("0.0") + ' ' + targetPosition.y.ToString("0.0") + ' ' + targetPosition.z.ToString("0.0");
         debugText.text = debugString;
-        return inkToControllerDistance + targetPosition; 
+        // return inkToControllerDistance + targetPosition; 
+        Vector3 newPosition = new Vector3(targetPosition.x, targetPosition.y, 1.7f);
+        Debug.Log(targetPosition + " | " + newPosition);
+        return newPosition;
     }
 
     private void Update()
