5af9ac15ba732128895c4fe04c1f1392017d4f1e
Jon Higgins <jon@jonhiggins.co.uk>
2018-09-15 22:17:21 +1000

    sync status UI to music BPM

--- Assets/Scripts/AppState.cs
+++ Assets/Scripts/AppState.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AppState : MonoBehaviour {
+public class AppState : MonoBehaviour
+{
 
     bool triangle = false;
     bool square = false;
@@ -19,8 +20,19 @@ public class AppState : MonoBehaviour {
     public Image squareImage;
     public Image circleImage;
 
+    public Animator triangleAnimator;
+    public Animator squareAnimator;
+    public Animator circleAnimator;
+
     Color inactiveColour = new Color(255, 255, 255, 0.5f);
     Color activeColour = new Color(255, 255, 255, 1);
+
+    double startTick;
+    double deltaDSP, oldDeltaDSP;
+
+    AnimatorStateInfo triangleAnimatorStateInfo;
+    AnimatorStateInfo squareAnimatorStateInfo;
+    AnimatorStateInfo circleAnimatorStateInfo;
 
     public void ToggleState(string shape)
     {
@@ -45,18 +57,60 @@ public class AppState : MonoBehaviour {
 
     private void Start()
     {
+        PlayAudio();
         StateChanged();
+    }
+
+    void PlayAudio()
+    {
+        startTick = AudioSettings.dspTime;
+        triangleAudioTrack.PlayScheduled(startTick);
+        circleAudioTrack.PlayScheduled(startTick);
+        squareAudioTrack.PlayScheduled(startTick);
     }
 
     // @TODO replace with broadcasting an event
     void StateChanged()
     {
-        //triangleAudioTrack.volume = triangle ? 1 : 0;
-        //squareAudioTrack.volume = square ? 1 : 0;
-        //circleAudioTrack.volume = circle ? 1 : 0;
+        triangleAnimatorStateInfo = triangleAnimator.GetCurrentAnimatorStateInfo(0);
+        circleAnimatorStateInfo = circleAnimator.GetCurrentAnimatorStateInfo(0);
+        squareAnimatorStateInfo = squareAnimator.GetCurrentAnimatorStateInfo(0);
+
+        triangleAudioTrack.volume = triangle ? 1 : 0;
+        squareAudioTrack.volume = square ? 1 : 0;
+        circleAudioTrack.volume = circle ? 1 : 0;
 
         triangleImage.color = triangle ? activeColour : inactiveColour;
         squareImage.color = square ? activeColour : inactiveColour;
         circleImage.color = circle ? activeColour : inactiveColour;
+
+        triangleAnimator.SetBool("active", triangle);
+        squareAnimator.SetBool("active", square);
+        circleAnimator.SetBool("active", circle);
+    }
+
+    void Update()
+    {
+        oldDeltaDSP = deltaDSP;
+        deltaDSP = AudioSettings.dspTime - startTick;
+
+        // Change time for currently played state
+        if (triangle)
+        {
+            triangleAnimator.PlayInFixedTime(0, 0, (float)(deltaDSP));
+        }
+
+        if (triangle)
+        {
+            circleAnimator.PlayInFixedTime(0, 0, (float)(deltaDSP));
+        }
+
+        if (square)
+        {
+            squareAnimator.PlayInFixedTime(0, 0, (float)(deltaDSP));
+        }
+
+
+
     }
 }
